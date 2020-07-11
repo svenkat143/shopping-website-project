7d8a11100c28112d57f9220ecec6649c7629e24a
svenkat143 <56305736+svenkat143@users.noreply.github.com>
2020-07-11 08:48:07 +0530

    Add files via upload

---
+++ jquery.js
@@ -0,0 +1,81 @@
+function check(form)
+      {
+       if(form.userid.value == "pinnu55@gmail.com" && form.pswrd.value == "prince")
+        {
+          window.open('home.html')
+          window.close('index.html')
+        } else if(form.userid.value == "pinnu555" && form.pswrd.value == "prince1"){
+          window.open('home.html')
+          window.close('index.html')
+        } else if(form.userid.value == "Gurdeepkumar880.gk@gmail.com" && form.pswrd.value == "4387285608"){
+          window.open('home.html')
+          window.close('index.html')
+        } else if(form.userid.value == "Gurdeepkumar880@gmail.com" && form.pswrd.value == "4387285608"){
+          window.open('home.html')
+          window.close('index.html')
+        } else if(form.userid.value == "Gurdeepgk96@gmail.com" && form.pswrd.value == "4387285608"){
+          window.open('home.html')
+          window.close('index.html')
+        } else if(form.userid.value == "Bharatnagpal1318@gmail.com" && form.pswrd.value == "4387285608"){
+          window.open('home.html')
+          window.close('index.html')
+        } else if(form.userid.value == "Bharatnagpal700@gmail.com" && form.pswrd.value == "4387285608"){
+          window.open('home.html')
+          window.close('index.html')
+        } else if(form.userid.value == "Bharatnagpal7837@gmail.com" && form.pswrd.value == "4387285608"){
+          window.open('home.html')
+          window.close('index.html')
+        } else if(form.userid.value == "Bharatnagpal783@gmail.com" && form.pswrd.value == "4387285608"){
+          window.open('home.html')
+          window.close('index.html')
+        } else if(form.userid.value == "Veekay697@gmail.com" && form.pswrd.value == "4387285608"){
+          window.open('home.html')
+          window.close('index.html')
+        } else if(form.userid.value == "Peterbrew60@gmail.com" && form.pswrd.value == "4387285608"){
+          window.open('home.html')
+          window.close('index.html')
+        } else if(form.userid.value == "Weshall697@gmail.com" && form.pswrd.value == "4387285608"){
+          window.open('home.html')
+          window.close('index.html')
+        } else if(form.userid.value == "Abhisheik4295@gmail.com" && form.pswrd.value == "4387285608"){
+          window.open('home.html')
+          window.close('index.html')
+        } else if(form.userid.value == "gurdeepkumar880.gk@gmail.com" && form.pswrd.value == "4387285608"){
+          window.open('home.html')
+          window.close('index.html')
+        } else if(form.userid.value == "gurdeepkumar880@gmail.com" && form.pswrd.value == "4387285608"){
+          window.open('home.html')
+          window.close('index.html')
+        } else if(form.userid.value == "gurdeepgk96@gmail.com" && form.pswrd.value == "4387285608"){
+          window.open('home.html')
+          window.close('index.html')
+        } else if(form.userid.value == "bharatnagpal1318@gmail.com" && form.pswrd.value == "4387285608"){
+          window.open('home.html')
+          window.close('index.html')
+        } else if(form.userid.value == "bharatnagpal700@gmail.com" && form.pswrd.value == "4387285608"){
+          window.open('home.html')
+          window.close('index.html')
+        } else if(form.userid.value == "bharatnagpal7837@gmail.com" && form.pswrd.value == "4387285608"){
+          window.open('home.html')
+          window.close('index.html')
+        } else if(form.userid.value == "bharatnagpal783@gmail.com" && form.pswrd.value == "4387285608"){
+          window.open('home.html')
+          window.close('index.html')
+        } else if(form.userid.value == "veekay697@gmail.com" && form.pswrd.value == "4387285608"){
+          window.open('home.html')
+          window.close('index.html')
+        } else if(form.userid.value == "peterbrew60@gmail.com" && form.pswrd.value == "4387285608"){
+          window.open('home.html')
+          window.close('index.html')
+        } else if(form.userid.value == "weshall697@gmail.com" && form.pswrd.value == "4387285608"){
+          window.open('home.html')
+          window.close('index.html')
+        } else if(form.userid.value == "abhisheik4295@gmail.com" && form.pswrd.value == "4387285608"){
+          window.open('home.html')
+          window.close('index.html')
+        }
+       else
+       {
+         alert("Error Password or Username")
+        }
+      }
